b263864832ecf3f71eca288c8b93e6ec2670f7ae
stzagkarak <stzagkarak@gmail.com>
2023-12-21 16:49:44 +0200

    Added actual Whiteroom window size

--- main.js
+++ main.js
@@ -2,16 +2,21 @@ process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
 const { app, BrowserWindow } = require('electron')
 
+const width = 1920 * 2;
+const height = 1200;
+
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 800/* 2240 + 1920 */,
-      height: 600/* 1080 */,
+      width: 600/* 2240 + 1920 */,
+      height: 800/* 1080 */,
       frame: false,
       resizable: false
     })
 
     win.loadFile('index.html')
     win.setPosition(0,0)
+    win.setSize(width, height)
+    win.setMinimumSize(width, height)
 }
 
 app.whenReady().then(() => {
